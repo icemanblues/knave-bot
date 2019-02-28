d83bdc35e56b6ae587a2b8d63cf8af5bd69b7f88
Roland Kluge <rolandkluge@yahoo.com>
2019-02-28 00:07:17 -0500

    adding a gin router to have some http endpoints

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println(Generate())
+
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/insult", func(c *gin.Context) {
+		c.String(200, "%s", Generate())
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
