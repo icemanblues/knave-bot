5792623c5277c7b784d027f4ed666a54414747d2
Roland Kluge <rolandkluge@yahoo.com>
2019-03-21 17:44:45 -0400

    adding new route for same insults. this will allow the api.rolandkluge.dev to use a new route philosophy. /service/version/resource

--- main.go
+++ main.go
@@ -12,11 +12,23 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 
-	r.GET("/api/v1/knave/insult", func(c *gin.Context) {
+	r.GET("/knavebot/v1/insult", func(c *gin.Context) {
 		c.String(200, "%s", Generate())
 	})
 
 	// https://api.slack.com/slash-commands
+	r.POST("/knavebot/v1/insult", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"text":          Generate(),
+			"response_type": "in_channel",
+		})
+	})
+
+	// OLD GET AND POST
+	// These are only here until the cutover is complete
+	r.GET("/api/v1/knave/insult", func(c *gin.Context) {
+		c.String(200, "%s", Generate())
+	})
 	r.POST("/api/v1/knave/insult", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"text":          Generate(),
